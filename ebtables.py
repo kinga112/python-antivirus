70da576e27a563be4853775e1ff0abe1ce37a08f
Alden <kinga112@yahoo.com>
2020-07-19 16:23:23 -0400

    updates

--- ebtables.py
+++ ebtables.py
@@ -5,6 +5,7 @@ def ebtables(device_ip, src_ip):
         os.system("sudo ebtables -A FORWARD -p IPv4 --ip-src {} --ip-dst {} -j ACCEPT".format(src_ip, dev_ip))
         os.system("sudo ebtables -A FORWARD -p IPv4 --ip-src {} --ip-dst {} -j ACCEPT".format(dev_ip, src_ip))
         os.system("sudo ebtables -A FORWARD -p IPv4 --ip-src {} -j DROP".format(device_ip))
+        os.system("sudo ebtables -A FORWARD -p IPv4 --ip-dst {} -j DROP".format(src_ip))
     except:
         return("Blocking new IP failed")
     return("Successfully blocked IP: {}".format(src_ip))
