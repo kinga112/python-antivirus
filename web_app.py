10d60c302666058b10ae6886fe13c2b3828ed61b
Alden <kinga112@yahoo.com>
2020-07-20 15:44:46 -0400

    updated table creation for device list

--- web_app.py
+++ web_app.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template, request
-from flask_table import Table, Col
 import mysql.connector
 from ebtables import ebtables
 
 app = Flask(__name__)
 
-@app.route('/') 
+@app.route('/')
 def home():
     return render_template('home.html')
 
@@ -15,15 +14,20 @@ def conf():
         dev_ip = request.form['ip']
         dev = request.form['devices']
         print('device ip: {}, device: {}'.format(dev_ip, dev))
-        # sql(dev_ip, dev)
+        sql(dev_ip, dev)
 
     if request.method == 'Get':
         print("get")
-        # stuff = ['1', '2', 't', 'a']
-    
-    return render_template('conf.html')
 
-def sql(dev_ip, dev):
+    try:
+        dev_ip_list, dev_list = get_sql()
+    except:
+        dev_ip_list = []
+        dev_list = []
+    
+    return render_template('conf.html', len=len(dev_ip_list), dev_ip_list=dev_ip_list, dev_list=dev_list)
+
+def add_sql(dev_ip, dev):
 
     mydb = mysql.connector.connect(
         host="localhost",
@@ -37,6 +41,16 @@ def sql(dev_ip, dev):
     val = (dev_ip, dev)
     mycursor.execute(sql, val)
     mydb.commit()
+
+def get_sql():
+    cursor.execute("SELECT device_ip, device FROM whitelist")
+    data = cursor.fetchall()
+    dev_ip_list = []
+    dev_list = []
+    for row in data:
+        dev_ip_list.append(row['device_ip'])
+        dev_list.append(row['device'])
+    return dev_ip_list, dev_list
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
