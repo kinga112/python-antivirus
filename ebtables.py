9332c823a588a36a6f59850cc761a2dfd7546a96
Alden <kinga112@yahoo.com>
2020-07-21 15:12:57 -0400

    changed os.system to subprocess

--- ebtables.py
+++ ebtables.py
@@ -1,15 +1,15 @@
-import os
+import subprocess
 
 def ebtables(device_ip, dev):
     f = open('devices/{}.txt'.format(dev), "r")
     for ip in f:
         ip.rstrip('\n')
         try:
-            os.system("sudo ebtables -A FORWARD -p IPv4 --ip-src {} --ip-dst {} -j ACCEPT".format(ip, device_ip))
-            os.system("sudo ebtables -A FORWARD -p IPv4 --ip-src {} --ip-dst {} -j ACCEPT".format(device_ip, ip))
-            os.system("sudo ebtables -A FORWARD -p IPv4 --ip-src {} -j DROP".format(device_ip))
-            os.system("sudo ebtables -A FORWARD -p IPv4 --ip-dst {} -j DROP".format(device_ip))
-            print("Successfully addded rules for Device: {}".format(device_ip))
+            subprocess.call("sudo ebtables -A FORWARD -p IPv4 --ip-src {} --ip-dst {} -j ACCEPT".format(ip, device_ip))
+            subprocess.call("sudo ebtables -A FORWARD -p IPv4 --ip-src {} --ip-dst {} -j ACCEPT".format(device_ip, ip))
+            subprocess.call("sudo ebtables -A FORWARD -p IPv4 --ip-src {} -j DROP".format(device_ip))
+            subprocess.call("sudo ebtables -A FORWARD -p IPv4 --ip-dst {} -j DROP".format(device_ip))
+            return 'none'
         except:
-            print("Adding rules for new device failed")
+            return 'error'
     
