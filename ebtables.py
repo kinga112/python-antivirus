48cf4d4a5251b8d5a14caf8685a4dd8114d4a135
Alden <kinga112@yahoo.com>
2020-07-21 16:40:20 -0400

    updates

--- ebtables.py
+++ ebtables.py
@@ -6,8 +6,8 @@ def add_rule(device_ip, dev):
         for ip in f:
             ip.rstrip('\n')
             print("sudo ebtables -A FORWARD -p IPv4 --ip-src {} --ip-dst {} -j ACCEPT".format(device_ip, ip))
-                subprocess.Popen(["sudo","ebtables","-A","FORWARD","-p","IPv4","--ip-src",device_ip,"--ip-dst",ip,"-j","ACCEPT"], stdout=subprocess.PIPE)
-                subprocess.Popen(["sudo","ebtables","-A","FORWARD","-p","IPv4","--ip-src",ip,"--ip-dst",device_ip,"-j","ACCEPT"], stdout=subprocess.PIPE)
+            subprocess.Popen(["sudo","ebtables","-A","FORWARD","-p","IPv4","--ip-src",device_ip,"--ip-dst",ip,"-j","ACCEPT"], stdout=subprocess.PIPE)
+            subprocess.Popen(["sudo","ebtables","-A","FORWARD","-p","IPv4","--ip-src",ip,"--ip-dst",device_ip,"-j","ACCEPT"], stdout=subprocess.PIPE)
         subprocess.Popen(["sudo","ebtables","-A","FORWARD","-p","IPv4","--ip-src",device_ip,"-j","DROP"], stfout=subprocess.PIPE)
         subprocess.Popen(["sudo","ebtables","-A","FORWARD","-p","IPv4","--ip-dst",device_ip,"-j","DROP"], stfout=subprocess.PIPE)
         return 'Ebtables successfully (probably not) added new rules'
@@ -17,7 +17,7 @@ def add_rule(device_ip, dev):
 def delete_rule(device_ip, dev):
     f = open('devices/{}.txt'.format(device_ip), "r")
     for ip in f:
-        ip.rstrip('\r\n')
+        ip.rstrip('\n')
         try:
             subprocess.Popen(["sudo","ebtables","-D","FORWARD","-p","IPv4","--ip-src",device_ip,"--ip-dst",ip,"-j","ACCEPT"], stdout=subprocess.PIPE)
             subprocess.Popen(["sudo","ebtables","-D","FORWARD","-p","IPv4","--ip-src",ip,"--ip-dst",device_ip,"-j","ACCEPT"], stdout=subprocess.PIPE)
